cb8fdd7ea4cf06ac06aedeee7e140ebea2004b50
kaua <kaua.santanaj@gmail.com>
2026-05-18 17:53:35 -0300

    adding config for webSocket

---
+++ src/main/java/com/infraLink/API/config/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.infraLink.API.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+
+
+
+}
